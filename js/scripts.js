0ad62dcd4e8697c916e94eb6c10efff63698af29
kenkbomb <146538181+kenkbomb@users.noreply.github.com>
2023-10-19 19:23:54 -0400

    task 1.9 changes

--- js/scripts.js
+++ js/scripts.js
@@ -2,17 +2,48 @@
 
 $("#button").click(function()
 {
-    let inputval =  $('#input').val();
-    $('#list').append('<li>'+inputval+'</li>');
-   
+    let li = $('<li></li>');
+    let inputval = $('#input').val();
+    li.append(inputval);
+
+
+    // let inputval =  $('#input').val();
+    let crossOutButton = $('<crossOutButton></crossOutButton>');
+    crossOutButton.append(document.createTextNode('X'));
+    $(crossOutButton).addClass('crossOutButton');
+    li.append(crossOutButton);
+    // inputval.append(crossOutButton);
     
+
+
+
+    $('#list').append(li);//adds the <li> element as the value taken from the form and also on this line, appends another 'button' element to the <li>....
+  
+
+        $('li').on('dblclick', function()
+    {
+        alert(this.innerText);
+        let listItem = $(this);
+        listItem.addClass('strike');
+
+    });
+    
+
+    $('crossOutButton').on('click', function()
+    {
+            
+        let listItem = $(this).parent();
+        listItem.addClass('delete');
+        alert('test');
+
+
+    });
+
+    $('#list').sortable();
 });
 
 
 
 
 
-$('.item').on('click', function()
-{
-    alert(this.innerText);
-});
+
